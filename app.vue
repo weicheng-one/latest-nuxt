30d3c0895bd6e6c77984646d132f3538d9c4567e
rayokdesign <hs960411@gmail.com>
2023-12-05 13:08:17 +0700

    update

--- app.vue
+++ app.vue
@@ -1,15 +1,12 @@
 <script setup>
 const { find } = useStrapi();
 
-const { data, pending, refresh, error } = await useAsyncData("posts", () =>
-  find("posts")
-);
+const { data, error } = await useAsyncData("posts", () => find("posts"));
 </script>
 <template>
   <div>
     <NuxtWelcome />
     <pre>{{ data }}</pre>
-    <pre>{{ pending }}</pre>
-    <pre>{{ error.message }}</pre>
+    <pre>{{ error }}</pre>
   </div>
 </template>

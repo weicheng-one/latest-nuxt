0a4053603170b8aac5b11d366b3a4e679ff9c060
rayokdesign <hs960411@gmail.com>
2023-12-05 12:52:41 +0700

    useAsyncData

--- app.vue
+++ app.vue
@@ -10,6 +10,6 @@ const { data, pending, refresh, error } = await useAsyncData("posts", () =>
     <NuxtWelcome />
     <pre>{{ data }}</pre>
     <pre>{{ pending }}</pre>
-    <pre>{{ error }}</pre>
+    <pre>{{ error.message }}</pre>
   </div>
 </template>

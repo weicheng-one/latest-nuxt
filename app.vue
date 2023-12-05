abcf116f06eaf856ad0c5f4b26218de97ebf9b90
rayokdesign <hs960411@gmail.com>
2023-12-05 12:20:38 +0700

    use strapi plugin

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup>
+const { find } = useStrapi();
+
+const response = await find("posts");
+</script>
 <template>
   <div>
     <NuxtWelcome />
+    {{ response }}
   </div>
 </template>
